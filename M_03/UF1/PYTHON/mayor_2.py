619c9f6bed74d5216bb51f3cb42ae686eb5cebf7
aperdiguerog <44448797+4lma@users.noreply.github.com>
2018-11-15 10:31:01 +0100

    Update and rename comparacion_2.py to mayor_2.py

---
+++ M_03/UF1/PYTHON/mayor_2.py
@@ -0,0 +1,20 @@
+entrada   sortrida
+-10-1-200   -1
+3 6 9       9
+3 9 6       9
+6 3 9       9
+6 9 3       9
+9 3 6       9
+9 6 3       9
+9 9 9       9
+9 9 6       9
+9 6 9       9 
+6 9 9       9 
+
+n1=int(input("introduce un número cualquiera"))
+n2=int(input("introduce un número cualquiera"))
+n3=int(input("introduce un número cualquiera"))
+if(n1>=n2>n3):
+  print (n1)
+else:
+  if(n1>=n3>n2):
