6c2e577a1eac27ed81db0ec53485429f6bbd22ad
aperdiguerog <44448797+4lma@users.noreply.github.com>
2018-11-15 09:40:56 +0100

    Create ejercicio-swap-4.py

---
+++ M_03/UF1/PYTHON/ejercicio-swap-4.py
@@ -0,0 +1,11 @@
+cajon1=input("que hay en el cajon 1?")
+cajon2=input("que hay en el cajon 2?")
+cajon3=input("que hay en el cajon 3?")
+cajon4=input("que hay en el cajon 4?")
+cajon5=cajon1
+cajon6=cajon2
+cajon1=cajon3
+cajon2=cajon4
+cajon3=cajon5
+cajon4=cajon6
+print (cajon1 , cajon2 , cajon3 , cajon4 )
